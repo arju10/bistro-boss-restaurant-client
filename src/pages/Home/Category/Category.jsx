c33b933724372ee86b5be49043feaddf953fb5ff
arju10 <arjuarju71@gmail.com>
2023-09-01 21:58:35 +0600

    Create shared Section Title for different sections

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -14,15 +14,22 @@ import slide2 from '../../../../assets/home/slide2.jpg';
 import slide3 from '../../../../assets/home/slide3.jpg';
 import slide4 from '../../../../assets/home/slide4.jpg';
 import slide5 from '../../../../assets/home/slide5.jpg';
+import SectionTitle from './../../../components/SectionTitle/SectionTitle';
 
 const Category = () => {
     return (
     
 
-        <Swiper
+        <section>
+             <SectionTitle
+            subHeading={"From 11.00am to 10.00pm"}
+            heading={"Order Online"}
+            ></SectionTitle>
+            <Swiper
             slidesPerView={4}
             spaceBetween={20}
             centeredSlides={true}
+            // slidesPerView = "auto"
             pagination={{
                 clickable: true,
             }}
@@ -52,6 +59,7 @@ const Category = () => {
             </SwiperSlide>
         </Swiper>
 
+        </section>
     );
 };
 
