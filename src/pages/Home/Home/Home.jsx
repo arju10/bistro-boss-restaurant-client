417e9c81a37acb07a542019b1f53a6087101fd71
arju10 <arjuarju71@gmail.com>
2023-09-01 22:29:22 +0600

    show popular menu items in the home page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,13 +1,18 @@
 
 
+
+import PopularMenu from '../../popularMenu/popularMenu';
 import Banner from '../Banner/Banner';
 import Category from '../Category/Category';
+
 
 const Home = () => {
     return (
         <div>
            <Banner/>
            <Category/>
+           <PopularMenu/>
+      
         </div>
     );
 };
---
+++ src/pages/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import { useEffect } from "react";
+import MenuItem from "../MenuItem/MenuItem";
+
+
+const PopularMenu = () => {
+    const [menu, setMenu] = useState([]);
+    useEffect(() => {
+      fetch("menu.json")
+      .then(res => res.json())
+      .then(data => {
+        const popularItems = data.filter(item => item.category === "popular");
+        setMenu(popularItems)
+      })
+    }, [])
+    
+    return (
+        <section className="mb-12">
+            <SectionTitle
+                heading="From Our Menu"
+                subHeading="Popular Items"
+            ></SectionTitle>
+            <div className="grid md:grid-cols-2 gap-10">
+                {
+                    menu.map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
+                }
+            </div>
+            <div className="flex justify-center items-center mt-4">
+                <button className="btn btn-outline border-0 border-b-4">View Full Menu</button>
+            </div>
+        </section>
+    );
+};
+
+export default PopularMenu;
