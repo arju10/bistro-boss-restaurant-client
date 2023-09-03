463270d7e33431650c4a4bec81370e2b569de5ee
arju10 <arjuarju71@gmail.com>
2023-09-04 01:16:56 +0600

    regex validation and sign up a user

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
 import { AuthContext } from '../../providers/AuthProvider';
+import { Helmet } from 'react-helmet-async';
 
 const Login = () => {
 
@@ -36,6 +37,10 @@ const Login = () => {
         }
     }
     return (
+        <>
+        <Helmet>
+            <title>Bistro Boss | Login</title>
+        </Helmet>
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col md:flex-row-reverse">
                 <div className="text-center md:w-1/2 lg:text-left">
@@ -43,7 +48,6 @@ const Login = () => {
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
                 <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
-                    
                     <form onSubmit={handleLogin} className="card-body">
                         <div className="form-control">
                             <label className="label">
@@ -62,19 +66,20 @@ const Login = () => {
                         </div>
                         <div className="form-control">
                             <label className="label">
-                            <LoadCanvasTemplate />
+                                <LoadCanvasTemplate />
                             </label>
                             <input type="text" ref={captchaRef} name="captcha" placeholder="type the captcha above" className="input input-bordered" />
                             <button onClick={handleValidateCaptcha} className="btn btn-outline btn-xs mt-2">Validate</button>
                         </div>
                         <div className="form-control mt-6">
-                        <input disabled={disabled} className="btn btn-primary" type="submit" value="Login" />
+                            <input disabled={disabled} className="btn btn-primary" type="submit" value="Login" />
                         </div>
                     </form>
                     <p><small>New Here? <Link to="/signup">Create an account</Link> </small></p>
                 </div>
             </div>
         </div>
+    </>
     );
 };
 
