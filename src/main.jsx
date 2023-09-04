962128dea91df89f8968c618e2c84a8b87961744
arju10 <arjuarju71@gmail.com>
2023-09-04 16:33:04 +0600

    use tan stack query to load data

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,12 @@ import {
 import { router } from "../src/Routes/Routes.jsx";
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './providers/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -14,9 +20,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
     <AuthProvider>
       <HelmetProvider>
-        <div className='max-w-screen-xl mx-auto'>
-          <RouterProvider router={router} />
-        </div>
+      <QueryClientProvider client={queryClient}>
+          <div className='max-w-screen-xl mx-auto'>
+            <RouterProvider router={router} />
+          </div>
+        </QueryClientProvider>
       </HelmetProvider>
     </AuthProvider>
 
