98bcd8ffe2e7b6b53313f22f3439038fa61685d8
arju10 <arjuarju71@gmail.com>
2023-09-04 18:57:47 +0600

    display cart data in a table

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -3,14 +3,15 @@ import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
     return (
-        <div className="drawer drawer-mobile">
+    <section>
+            <div className="drawer drawer-mobile bg-[#D1A054]">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col items-center justify-center">
                 <Outlet />
                 <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 
             </div>
-            <div className="drawer-side bg-[#D1A054]">
+            <div className="drawer-side ">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80">
 
@@ -26,6 +27,7 @@ const Dashboard = () => {
 
             </div>
         </div>
+    </section>
     );
 };
 
--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,22 +1,66 @@
 import { Helmet } from "react-helmet-async";
 import useCart from "../../../hook/useCart";
+import { FaTrashAlt } from "react-icons/fa";
 
 const MyCart = () => {
     const [cart] = useCart();
     console.log(cart);
     const total = cart.reduce((sum, item) => item.price + sum, 0)
-    
+
     return (
         <div>
-        <Helmet>
-            <title>Bistro Boss | My Cart</title>
-        </Helmet>
-        <div className="uppercase">
-            <h3 className="text-3xl">Total Items: {cart.length}</h3>
-            <h3 className="text-3xl">Total Price: ${total}</h3>
-            <button className="btn btn-warning btn-sm">PAY</button>
+            <Helmet>
+                <title>Bistro Boss | My Cart</title>
+            </Helmet>
+           
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Food</th>
+                            <th>Item Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item, index) => <tr
+                                key={item._id}
+                            >
+                                <td>
+                                    {index + 1}
+                                </td>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {item.name}
+                                </td>
+                                <td className="text-end">${item.price}</td>
+                                <td>
+                                    <button className="btn btn-ghost bg-red-600 btn-lg text-white"><FaTrashAlt></FaTrashAlt></button>
+                                </td>
+                            </tr>)
+                        }
+
+
+                    </tbody>
+                </table>
+            </div>
+
+            <div className="uppercase font-semibold h-[60px] flex justify-evenly items-center">
+                <h3 className="text-3xl">Total Items: {cart.length} </h3>
+                <h3 className="text-3xl">Total Price: ${total}</h3>
+                <button className="btn btn-warning btn-sm">PAY</button>
+            </div>
         </div>
-    </div>
     );
 };
 
