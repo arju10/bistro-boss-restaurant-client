c58d0ec66d60c8cec9159a6f8fe77b1fc21fcb5d
arju10 <arjuarju71@gmail.com>
2023-09-10 07:10:32 +0600

    calculate total price to be charged to the credit card

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -2,18 +2,24 @@ import { Elements } from "@stripe/react-stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import CheckoutForm from "./CheckoutForm";
 import { loadStripe } from "@stripe/stripe-js";
+import useCart from './../../../hook/useCart'
+
 
 const Payment = () => {
+    const [cart] = useCart();
+    const total = cart.reduce((sum, item) => sum + item.price, 0);
+    const price = parseFloat(total.toFixed(2))
     // TODO: provide publishable Key
-const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
+    const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
     return (
+
         <div>
-        <SectionTitle subHeading="please process" heading="Payment"></SectionTitle>
-        <h2 className="text-3xl"> Teka o teka tumi uira uira aso...</h2>
-        <Elements stripe={stripePromise}>
-            <CheckoutForm/>
-        </Elements>
-    </div>
+            <SectionTitle subHeading="please process" heading="Payment"></SectionTitle>
+            <h2 className="text-3xl"> Teka o teka tumi uira uira aso...</h2>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm price={price} />
+            </Elements>
+        </div>
     );
 };
 
