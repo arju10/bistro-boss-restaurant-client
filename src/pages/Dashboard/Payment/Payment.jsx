be24b4550cee7c0338173bdbba6c70a982301f71
arju10 <arjuarju71@gmail.com>
2023-09-10 06:45:40 +0600

    set payment gateway publishable key

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,11 +1,19 @@
+import { Elements } from "@stripe/react-stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import CheckoutForm from "./CheckoutForm";
+import { loadStripe } from "@stripe/stripe-js";
 
 const Payment = () => {
+    // TODO: provide publishable Key
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
     return (
         <div>
-            <SectionTitle subHeading="please process" heading="Payment"></SectionTitle>
-            <h2 className="text-3xl"> Teka o teka tumi uira uira aso...</h2>
-        </div>
+        <SectionTitle subHeading="please process" heading="Payment"></SectionTitle>
+        <h2 className="text-3xl"> Teka o teka tumi uira uira aso...</h2>
+        <Elements stripe={stripePromise}>
+            <CheckoutForm/>
+        </Elements>
+    </div>
     );
 };
 
